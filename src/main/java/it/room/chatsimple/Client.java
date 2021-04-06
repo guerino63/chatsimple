80c314167040727c1c8abd31046e2b95be88953c
ma <guerinmeschino@gmail.com>
2021-04-06 11:31:04 +0200

    finale 1.0

--- src/main/java/it/room/chatsimple/Client.java
+++ src/main/java/it/room/chatsimple/Client.java
@@ -13,72 +13,112 @@ import javax.swing.JOptionPane;
  * @author ma
  */
 public class Client {
-    
+
     String nomeUtente;  //Nome dell' utente
     String ip;          //Indirizzo di rete
     int porta;          //porta
 
+    /**
+     * Costruttore N° 1 - senza parametri, pone a default le variabili di classe
+     */
     public Client() {
         nomeUtente = "Luisa Bianchi";
         ip = "127.0.0.1";           //localhost
         porta = 6868;
     }
 
+    /**
+     * Costruttore N° 2 - Parametrizzata, la sua istanza permette di specificare
+     * tutti i parametri di connessione al Server
+     *
+     * @param nomeUtente
+     * @param ip
+     * @param porta
+     */
     public Client(String nomeUtente, String ip, int porta) {
         this.nomeUtente = nomeUtente;
         this.ip = ip;
         this.porta = porta;
     }
-    
-    public void init() throws IOException{
-        
+
+    public void init() throws IOException {
+
         print("**********************************");
         print("**         C L I E N T          **");
         print("**********************************");
         /*
-        Cerchiamo un server attivo. Il programma rimane in attesa finche' non troviamo
-        un Server disponibile all' indirizzo ip:porta
+        Cerchiamo un server attivo all' indirizzo ip:porta
+        Se non lo trova, il metodo uscira' generando una eccezione di IO(Connessione rifiutata (Connection refused)).
+        Il che costringerà a rieseguire il metodo di nuovo.
+        Prima però assicuratevi che un Server sia attivo sul IP:porta, altrimenti uscirà di nuovo con errore IO
          */
         print("Provo a connettermi con un server all' indirizzo: " + ip + ":" + porta);
         Socket clientSocket = new Socket(ip, porta);    //Proviamo a cercare un Server attivo
         print("Connessione al server effettuata!");
-        
+
         //Istanze delle classe per flussi di spedizione-ricezione dati attraverso il socket
         DataOutputStream spedisci = new DataOutputStream(clientSocket.getOutputStream());
         DataInputStream ricevi = new DataInputStream(clientSocket.getInputStream());
 
-        while(true){
+        /*
+        Loop di digitazione messaggi verso il Server
+        Non si esce da qui, finche' non si digita esce
+         */
+        while (true) {
             //Attendiamo messaggio da Server
             String readUTF = ricevi.readUTF();
             print(readUTF); //Print output del messaggio ricevuto dal Server
-            String readLine = JOptionPane.showInputDialog(null, "Messaggio da spedire al Server...","CLIENT",JOptionPane.QUESTION_MESSAGE);
+            //Richiesta input di un messaggio attraverso una Dialog Box
+            String readLine = JOptionPane.showInputDialog(null, "Messaggio da spedire al Server...", "CLIENT", JOptionPane.QUESTION_MESSAGE);
             String ilClientDice = spedisciString(readLine);
             spedisci.writeUTF(ilClientDice);
             printEcho(readLine);
-            if(readLine.toLowerCase().contains("esci")){ //Se il messaggio e' "esci", terminiamo il Server uscendo dal loop
+            if (readLine.toLowerCase().contains("esci")) { //Se il messaggio e' "esci", terminiamo il Server uscendo dal loop
                 break;
             }
-            
+
         }
-        
+
         //Chiudi i flussi
         spedisci.close();
         ricevi.close();
         clientSocket.close();
     }
-    
 
-    private String spedisciString(String st){
-        String stDiRitorno = String.format("[%s] dice:%s",nomeUtente,st);
+    /**
+     * Aggiungiamo in testa al parametro st, il nomeUtente e la dicitura:"
+     * dice:";
+     *
+     * @param st
+     * @return
+     */
+    private String spedisciString(String st) {
+        String stDiRitorno = String.format("[%s] dice:%s", nomeUtente, st);
         return stDiRitorno;
     }
-    private void print(String st){
-        System.out.println("Client:"+st);
+
+    /**
+     * Aggiungiamo in testa al parametro st la Stringa "Client:"
+     * @param st 
+     */    
+    private void print(String st) {
+        System.out.println("Client:" + st);
     }
-    private void printEcho(String st){
-        System.out.println("\t>>:"+st);
-    }    
+
+    /**
+     * Aggiungiamo in testa al parametro st una tabulazione(\t) per identificare meglio 
+     * il messaggio spedito al Server.
+     * @param st 
+     */
+    private void printEcho(String st) {
+        System.out.println("\t>>:" + st);
+    }
+
     
+    /**
+     * Punto di start della classe Client
+     * @param args 
+     */
     public static void main(String[] args) {
         Client client = new Client();
         try {
--- src/main/java/it/room/chatsimple/Server.java
+++ src/main/java/it/room/chatsimple/Server.java
@@ -10,16 +10,22 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
- *
+ * IL SERVER
+ * Classe che 'ascolta ed aspetta' che in rete una classe Client chieda di essere ascoltata.
+ * L' ascolto in rete si effettua usando un 
+ * indirizzo IP(indirizzo internet) ed una porta(meglio se compresa tra 6000 e 65000) che 
+ * Server e Client conoscono e condividono
+ * 
  * @author ma
  */
 public class Server {
     
-    String nomeUtente;  //Nome dell' utente Server
-    int porta;          //porta del server
+    //Variabili di classe - private(Solo la classe Server puo' vedere queste classi)
+    private String nomeUtente;  //Nome dell' utente Server
+    private int porta;          //porta del server
 
     /**
-     * Costruttore 1 - variabili di classe di default
+     * Costruttore 1 - Le variabili di classe avranno valori di default
      */
     public Server() {
         nomeUtente = "Mario Rossi";
@@ -27,7 +33,8 @@ public class Server {
     }
 
     /**
-     * Costruttore N°2 - 
+     * Costruttore N°2 - Il costruttore ha come parametri nomeUtente e porta
+     * Se usiamo questa istanza possamo quindi cambiare alcune variabili di classe.
      * @param nomeUtente
      * @param porta 
      */
@@ -42,27 +49,40 @@ public class Server {
         print("**********************************");
         
         //Crea l' istanza della classe ServerSocket
+        //La classe ServerSocket si mette in ascolto all' indirizzo IP attuale sulla porta
+        //definita nella variabile di classe porta
+        //L' indirizzo IP e' quello che vi assegna il vostro provider(TIM-VODAFONE...etc) 
+        //Quando vi collegate alla rete.
+        //Per scoprirlo usate uno dei tanti siti che vi ritorna il vostro IP
+        //@see http://www.ilmioindirizzoip.it/
         ServerSocket sv = new ServerSocket(porta);
         
         print("Attendo connessione al client...");        
-        Socket accept = sv.accept();
+        Socket accept = sv.accept();    //Da qui il programma non si schioda finche non sentiamo 'bussare' un Client
         print(String.format("Connessione ad un client effettuata! [Client=%s]",sv.getLocalSocketAddress().toString()));
 
-        //Istanze delle classe per flussi di spedizione-ricezione dati attraverso il socket
+        //Se siamo qui, un Client si è connesso...
+        //Creo istanze delle classi per flussi di spedizione-ricezione dati attraverso il socket
         DataOutputStream spedisci = new DataOutputStream(accept.getOutputStream());
         DataInputStream ricevi = new DataInputStream(accept.getInputStream());
         
-        //Spedisci messaggio di benvenuto al Client...
+        //Spediamo messaggio di benvenuto al Client...
         String ilServerDice = spedisciString("Benvenuto!");
         spedisci.writeUTF(ilServerDice);
         printEcho("Benvenuto!");
-        while(true){
+        
+        /*
+        Loop di spedizione messaggi verso Client.
+        Si esce quando si riceve dal Client il messaggio "esci"
+        */
+        while(true){ 
             //Attendiamo messaggio da Client
             String readUTF = ricevi.readUTF();
             print(readUTF); //Print output del messaggio ricevuto dal Client
             if(readUTF.toLowerCase().contains("esci")){ //Se il messaggio è "esci", terminiamo il Server uscendo dal loop
                 break;
             }
+            //Richiesta input di un messaggio da pedire al Client
             String readLine = JOptionPane.showInputDialog(null, "Messaggio da spedire al Client...","SERVER",JOptionPane.QUESTION_MESSAGE);
             ilServerDice = spedisciString(readLine);
             spedisci.writeUTF(ilServerDice);
@@ -75,19 +95,38 @@ public class Server {
         accept.close();
         sv.close();
     }
+    
+    /**
+     * Aggiungiamo in testa al parametro st, il nomeUtente e la dicitura:" dice:";
+     * @param st
+     * @return 
+     */
     private String spedisciString(String st){
         String stDiRitorno = String.format("[%s] dice:%s",nomeUtente,st);
         return stDiRitorno;
     }
+    /**
+     * Aggiungiamo in testa al parametro st la Stringa "Server:"
+     * @param st 
+     */
     private void print(String st){
         System.out.println("Server:"+st);
     }
+    /**
+     * Aggiungiamo in testa al parametro st una tabulazione(\t) per identificare meglio 
+     * il messaggio spedito al Client.
+     * @param st 
+     */
     private void printEcho(String st){
         System.out.println("\t>>:"+st);
     }
     
     
     
+    /**
+     * Punto di start della classe Server
+     * @param args 
+     */
     public static void main(String[] args) {
         Server server = new Server();
         try {
